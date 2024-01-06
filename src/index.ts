0b2aa97315c2ccc5a14a6251e798cbf6a98af712
TranNgocChinhIT <chinhtoan127@gmail.com>
2024-01-06 20:33:45 +0700

    finish format

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import { Elysia } from "elysia"
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000)
 
-console.log(`   
-                                                              🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
